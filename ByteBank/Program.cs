6711e8150f7a4ea689c4223d0dc6c148ac86815d
Tiago Barbosa <tiago@farias.cc>
2021-10-26 21:09:38 -0300

    Journey CSharp Part04 - Entendendo exceções

---
+++ ByteBank/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ByteBank
+{
+  static class Program
+  {
+    static void Main(string[] args)
+    {
+      try
+      {
+        Metodo();
+      }
+      catch (DivideByZeroException)
+      {
+        Console.WriteLine("Não é possível divisão por 0!");
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e.Message);
+        Console.WriteLine(e.StackTrace);
+      }
+    }
+
+    private static void Metodo()
+    {
+      TestaDivisao(0);
+    }
+
+    private static void TestaDivisao(int divisor)
+    {
+      Dividir(10, divisor);
+    }
+
+    private static int Dividir(int numero, int divisor)
+    {
+      try
+      {
+        return numero / divisor;
+      }
+      catch (Exception)
+      {
+        Console.WriteLine($"Exceção com número = {numero} e divisor = {divisor}");
+        throw;
+      }
+    }
+  }
+}
